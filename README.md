92ac139d12292afedc5876c03bbd3c290a271309
HelloLeeChan <iwar3@qq.com>
2016-04-06 13:12:46 +0800

    commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Algorithm-Code-notes
 The exercise of Algorithm
-碰到的试题都试着多种方法重写一遍，
+碰到的试题都试着多种方法重写一遍！
--- The Longest Palindrome.js
+++ The Longest Palindrome.js
@@ -2,16 +2,20 @@
  * Created by licheng on 16/4/5.
  */
 
-var jishu = 0
+
 function findPalindrome(str){
+    // extend String method to reverse the character like Array
     String.prototype.reverse = function(){
         return this.split('').reverse().join('')
     }
-    String.prototype.splice = function(i,n,c){
+    // extend String method to splice(delete) the character like Array
+    String.prototype.splice = function(index,num,insert){
         var that = this.split('')
-        that.splice(i,n,c)
+        that.splice(index,num,insert)
         return that.join('')
     }
+
+    //if a string is a palindrome
     function judge(str){
         if(str === str.reverse()){
             return true
@@ -20,10 +24,12 @@ function findPalindrome(str){
             return false
         }
     }
+    // wether to continue the function
     var  stop = false
 
+    // the function to enumerate the charater to be deleted
+    // c is the number of characters to be deleted
     function del(str,c){
-   // console.log(jishu++)
         if(c ===0 && !stop){
             if(judge(str)){
                 stop = true
@@ -31,7 +37,6 @@ function findPalindrome(str){
                 return str.length
             }
         }
-
         else if(c === 1 && !stop){
             var string = str
             var strlen = str.length
@@ -46,31 +51,28 @@ function findPalindrome(str){
             }
         }
         else if(c > 1 && !stop){
-            //ex
             var str = str
             var len = str.length
             for(var j = 0;j<len&&!stop;j++){
                 // console.log(j)
                 var count = c
                 mystr = str.splice(j,1)
-               // console.log('C: '+count+'str: '+mystr+' J: '+j)
+               // console.log('Count: '+count+'str: '+mystr+' J: '+j)
                 del(mystr,--count)
 
             }
         }
         else{
-            var num = 1
-            console.log ('end')
             throw new error('error argument')
         }
     }
 
+//enumerate the number of characters to be deleted
     var i = 0
     while (i < str.length && !stop ){
             del(str,i)
         i++
-       // console.log('last:'+i)
     }
 }
 
-findPalindrome('abbacc')
+findPalindrome('abbaccsf')
